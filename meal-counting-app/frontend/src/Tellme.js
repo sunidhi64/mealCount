e4a4742d6bcff46477cd3fa7f5794574257dcdf3
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-07-22 00:55:08 +0530

    committed

--- meal-counting-app/frontend/src/Tellme.js
+++ meal-counting-app/frontend/src/Tellme.js
@@ -1,33 +1,34 @@
-import React from "react"
+mport React from "react"
 import "./Tellme.css"
 
 
 function Tellme() {
 	    return(
-	        <body>
+		            <body>
 		                
-		    <header className="v-header container">
+		                <header className="v-header container">
 		                    
-		        <div className="fullscreen-video-wrap">
-		            <video  autoPlay loop muted className="video-bg">
-		                <source src={"https://media.istockphoto.com/videos/teenage-students-being-served-meal-in-school-canteen-video-id942400270"} type="video/mp4" /> 
-		            </video> 
-		        </div>
-		        <div className="header-overlay"></div>
+		                    <div className="fullscreen-video-wrap">
+		                        <video  autoPlay loop muted className="video-bg">
+		                        <source src={"https://media.istockphoto.com/videos/teenage-students-being-served-meal-in-school-canteen-video-id942400270"} type="video/mp4" /> 
+		                        </video> 
+		                    </div>
+		                    <div className="header-overlay"></div>
 		            
-		        <div className="header-content">
-		            <pre>  Meal Counting App</pre>
-		            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-		            <div className="WhoRu">
-		                <h2>Let's Get Started</h2>
-		                <div className="StuLogin">
-		                <a href="./StudentLogin">Student?</a></div><br/><div className="MessLogin"><a  href="./MessReg">Mess Authority?</a></div>
-		            </div>
-		        </div>
+		                    <div className="header-content">
+		                        <pre>  Meal Counting App</pre>
+		                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		                        <div className="WhoRu">
+		                            <h2>Let's Get Started</h2>
+		                            <div className="StuLogin">
+		                            <a href="./StudentLogin">Student?</a></div><br/><div className="MessLogin"><a  href="./MessReg">Mess Authority?</a></div>
+		                        </div>
+		                    </div>
 
-		    </header>
-		</body>
-            )
-        }
+		                </header>
+		            </body>
+		           
+		            
+		        )
+}
 export default Tellme
-
