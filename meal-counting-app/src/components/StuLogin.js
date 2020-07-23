e05acfffe5f869fe0699b589248a4e1aa27f31b6
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-07-23 22:40:09 +0530

    committed

---
+++ meal-counting-app/src/components/StuLogin.js
@@ -0,0 +1,28 @@
+import React, { Components } from "react"
+import "./StuLogin.css"
+class StuLogin extends Components {
+	constructor() {
+		super() 
+			this.state={
+				username = ""
+				password = ""
+			}
+			this.handleChange=this.handleChange.bind(this)
+	}
+	handleChange(event){
+		this.setState({
+			[event.target.name]:event.target.value
+		})
+	}
+	render() {
+		return(
+			<body>
+			  <div class="loginbox">
+			  <img src="https://img.pngio.com/app-icon-set-login-icon-svg-png-icon-free-download-311846-login-icon-png-980_980.png">
+			  </div>
+			</body>
+
+		)
+	}
+	
+}
