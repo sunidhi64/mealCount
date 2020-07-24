e0f97446fa0f80f38cd10524623c5fde97f7d386
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-07-24 18:06:39 +0530

    commit

---
+++ meal-counting-app/src/components/messauth2.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import "./messauth2.css"
+class Messauth2 extends Component {
+	constructor() {
+	    super()
+	    this.state = {
+		username: "",
+		password: ""
+	    }
+	    this.handleChange=this.handleChange.bind(this)
+	}
+
+	handleChange(event){
+	    this.setState({
+		[event.target.name]: event.target.value
+	    })
+	}
+	render(){
+	    return(
+	      	   <body>
+		    <div class="container
+		   </body>
+}
