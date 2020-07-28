1adad210b71218c50be073e9720d1f632073f88e
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-07-28 17:13:40 +0530

    commited

--- meal-counting-app/src/components/StuLogin.js
+++ meal-counting-app/src/components/StuLogin.js
@@ -25,7 +25,27 @@ class StuLogin extends Component {
     render() {
         return(
             <div>
-                <h1>helloworld</h1>
+                <body className="StuLogin2">
+                    <div className="StuLogin2-container" id="StuLogin2-container">
+                        <div className="Stu-form-container Stu-sign-in-container">
+					    <form action="#" >
+		    			    <h1 >Login</h1><br/>
+		    			    <input type="text" placeholder="User Name" name="username" onChange={this.handleChange}/>
+		    			    <input type="password" placeholder="Password" name="password" onChange={this.handleChange}/>
+		    			    <br/><button className="StuLogin2-button">Login</button>
+		    		    </form>
+		    	        </div>
+                    
+                    <div class="Stu-overlay-container">
+                        <div class="Stu-overlay">
+                        <div class="Stu-overlay-panel Stu-overlay-right">
+		    				<h1>Hello!</h1>
+		    				<p>To keep connected with us please login with your personal info</p>
+		    			</div>
+                        </div>
+                    </div>
+					</div>
+                </body>
             </div>
         )
     }
