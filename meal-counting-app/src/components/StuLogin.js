58df3db264f103bcd7dd62410225cd4224cef910
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-07-28 16:03:09 +0530

    committed

---
+++ meal-counting-app/src/components/StuLogin.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import "./StuLogin.css";
+
+
+
+class StuLogin extends Component {
+	constructor() {
+	    super()
+	    this.state = {
+		username: "",
+		password: ""
+		
+	    }
+		this.handleChange=this.handleChange.bind(this);
+		this.handleLogin=this.handleLogin.bind(this);
+		this.handleSubmit=this.handleSubmit.bind(this);
+		
+	}
+	
+	handleChange(event){
+	    this.setState({
+		[event.target.name]: event.target.value
+	    })
+    }
+    render() {
+        return(
+            <div>
+                <body class="StuLogin">
+                    <div class="StuLogin-container" id="StuLogin-container">
+                        <div class="Stu-form-container Stu-sign-in-container">
+					    <form action="#" onSubmit={this.handleLogin}>
+		    			    <h1 >Login</h1><br/>
+		    			    <input type="text" placeholder="User Name" name="username" onChange={this.handleChange}/>
+		    			    <input type="password" placeholder="Password" name="password" onChange={this.handleChange}/>
+		    			    <br/><button>Login</button>
+		    		    </form>
+		    	        </div>
+                    </div>
+                    <div class="Stu-overlay-container">
+                        <div class="Stu-overlay">
+                        <div class="Stu-overlay-panel Stu-overlay-right">
+		    				<h1>Hello!</h1>
+		    				<p>Enter your personal details and start journey with us</p>
+		    				
+		    			</div>
+                        </div>
+                    </div>
+
+                </body>
+            </div>
+        )
+    }
+
+}
+export default StuLogin
