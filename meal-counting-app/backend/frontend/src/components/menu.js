9e76173650a4b037736b6bd46d37845408cd1072
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-08-15 17:28:39 +0530

    committed

--- meal-counting-app/backend/frontend/src/components/menu.js
+++ meal-counting-app/backend/frontend/src/components/menu.js
@@ -1,9 +1,64 @@
 import React, { Component } from "react";
-
+import "./menu.css"
 export default class Menu extends Component {
     render() {
         return (
-            <h1>Hi, there!</h1>   
+            <body className="student-menu">
+            <div>
+                <table>
+                    
+                    <tr>
+                        <th className="meal-menu" colSpan="6" >Meal Menu</th>
+                    </tr>
+                    <tr>
+                        <th ></th>
+                        <th >Veg</th>
+                        <th >Non-veg</th>
+                        <th >Yes</th>
+                        <th >No</th>
+                    </tr>
+                    
+                    <tr>
+                        <th >Breakfast</th>
+                    
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        
+                        
+                        
+                    </tr>
+                    <tr>
+                        <th >Lunch</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        
+                        
+                    </tr>
+                    <tr>
+                        <th >Snacks</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        
+                    </tr>
+                    <tr>
+                        <th >Dinner</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        
+                        
+                    </tr>
+                    
+                </table>
+            </div>
+            </body>
         )
     }
 }
