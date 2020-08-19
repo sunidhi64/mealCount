ca2447c7b11832018e0206ef3d637d5a377301e2
Shaik_Sabiha_Tarannum <sabihatarannum2002@gmail.com>
2020-08-20 01:26:46 +0530

    committed

--- meal-counting-app/backend/frontend/src/components/menu.js
+++ meal-counting-app/backend/frontend/src/components/menu.js
@@ -4,112 +4,46 @@ export default class Menu extends Component {
     render() {
         return (
             <body className="student-menu">
-            <div style={{overflowY:"auto"},{overflowX:"auto"}}>
-  <table className="center">
-      
-    
-      <tr><th colSpan="8" style={{textAlign:"center"}}>Meal Menu</th></tr>
-      <tr><th colSpan="8" style={{textAlign:"center"}}>Veg</th></tr>
-      
-      <tr>
-      <th>Meals</th>
-      <th>Monday</th>
-      <th>Tuesday</th>
-      <th>Wednesday</th>
-      <th>Thursday</th>
-      <th>Friday</th>
-      <th>Saturday</th>
-      <th>Sunday</th>
-    </tr>
-    
-    <tr>
-        <th>Breakfast</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    <tr>
-        <th>Lunch</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    <tr>
-        <th>Snacks</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    <tr>
-        <th>Dinner</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    
-    <tr>
-        <th colSpan="8" style={{textAlign:"center"}}>Non-veg</th>
-    </tr>
-    <tr>
+    <div>
+    <table class="center" width="50%">
+            <tr><th colSpan="5" style={{fontSize:"20px"}}>Weekly Meal Menu</th></tr>
+  <tr>
+    <th rowSpan="5" ><ol><li><button className="monday">Monday</button></li><br/><li><button className="tuesday">Tuesday</button></li><br/><li><button className="wednesday">Wednesday</button></li><br/><li><button className="thursday">Thursday</button></li><br/><li><button className="friday">Friday</button></li><br/><li><button className="saturday">Saturday</button></li><br/><li><button className="sunday">Sunday</button></li></ol></th>
     <th>Breakfast</th>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-    <td><ul><li></li><li></li><li></li></ul></td>
-        
-    </tr>
-    <tr>
-        <th>Lunch</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    <tr>
-        <th>Snacks</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-    <tr>
-        <th>Dinner</th>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-        <td><ul><li></li><li></li><li></li></ul></td>
-    </tr>
-  </table>
-</div>
-</body>
-        )
+    <th>Lunch</th>
+    <th>Snacks</th>
+    <th>Dinner</th>
+</tr>
+<tr>
+    <th>Veg</th>
+    <th>Veg</th>
+    <th>Veg</th>
+    <th>Veg</th>
+    
+</tr>
+<tr>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    
+</tr>
+<tr>
+    <th>NonVeg</th>
+    <th>NonVeg</th>
+    <th>NonVeg</th>
+    <th>NonVeg</th>
+</tr>
+<tr>
+<td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+    <td><ol type="1"><li></li><li></li><li></li><li></li><li></li></ol></td>
+</tr>
+</table>
+
+    </div>
+    </body>
+    )
     }
 }
