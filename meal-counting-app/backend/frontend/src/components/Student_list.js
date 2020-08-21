31cae81092148f5491b199c8f5cac28e6b81e454
simlibanerjee14@gmail.com <simlibanerjee14@gmail.com>
2020-08-21 23:35:51 +0530

    committed

---
+++ meal-counting-app/backend/frontend/src/components/Student_list.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./student_list.css";
+import axios from "axios";
+export default class Student_list extends Component {
+    render(){
+        return (
+            <body>
+                <table className = "Student_list_table" width = "50%">
+                    <tr>
+                        <th>Student Name</th>
+                        <th> Registration No.</th>
+                        <th> Room No.</th>
+                    </tr>
+                    <tr> 
+                        <td>Simli</td> 
+                        <td> 868662787</td>
+                        <td> 123</td>
+                    </tr>
+                </table>
+            </body>
+        )
+    }
+}
+render
