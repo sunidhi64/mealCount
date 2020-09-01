c1d99c68e026cc1dd8cddd7ef5cc0f5f8227e07c
Sunidhi Sharma <19ucs087@lnmiit.ac.in>
2020-09-01 10:08:08 +0530

    added main page

--- meal-counting-app/backend/frontend/src/components/Tellme3.js
+++ meal-counting-app/backend/frontend/src/components/Tellme3.js
@@ -5,6 +5,12 @@ import students from "./students.png";
 import cookicon  from "./cookicon.png";
 import m  from "./m.jpg"
 export default class Tellme3 extends Component{
+    handleStudent(){
+        window.location = "/stuLogin";
+    }
+    handleMessauth(){
+        window.location = "/messauth/login";
+    }
     render(){
         return(
             <body>
@@ -14,9 +20,9 @@ export default class Tellme3 extends Component{
                 </div>
                 <div className="right-side">
                     <img src={students} width="200px"className="students"></img>
-                    <button className="Student">Student</button>
+                    <button className="Student" onClick={this.handleStudent}>Student</button>
                     <img src={cookicon} width="200px" className="cooks"></img>
-                    <button className="Messauth">Mess Authority</button>
+                    <button className="Messauth"onClick={this.handleMessauth}>Mess Authority</button>
                 </div>
             </body>
         )
